25663c053f821d9808b75452db254ceac21b053a
Andrii Sereda <a.a.sereda@ukr.net>
2022-11-29 17:08:06 +0200

    fixing error hidding spinner backdrop

--- src/js/api/fetchApiMovies.js
+++ src/js/api/fetchApiMovies.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import { hiddenSpinner } from '../components/hiddenComponents/spinnerMode';
+
 const API_KEY = '20e13b9a04a0b41c58cfa9dd598a635c';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const URL_TRENDING = 'trending/movie/day';
@@ -7,103 +9,110 @@ const URL_KEY = 'search/movie';
 const URL_ID = 'movie';
 
 const searchParams = new URLSearchParams({
-  api_key: API_KEY,
-  language: 'en-US',
-  page: 1,
-  include_adult: false,
+  api_key: API_KEY,
+  language: 'en-US',
+  page: 1,
+  include_adult: false,
 });
 
 export class FetchApiMovies {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-    this.loadedHits = 0;
-  }
-  async fetchWithPage(pageNumber) {
-    const searchParams = new URLSearchParams({
-      api_key: API_KEY,
-      language: 'en-US',
-      page: pageNumber,
-      include_adult: false,
-    });
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.loadedHits = 0;
+  }
+  async fetchWithPage(pageNumber) {
+    hiddenSpinner(false);
+    const searchParams = new URLSearchParams({
+      api_key: API_KEY,
+      language: 'en-US',
+      page: pageNumber,
+      include_adult: false,
+    });
 
-    const url = `${BASE_URL}${URL_TRENDING}?${searchParams}`;
-    console.log(url);
-    try {
-      const response = await axios.get(url);
-      this.setQuery('');
-      this.resetPage();
-      this.setLoadedHits(response.data.total_pages);
-      return response.data;
-    } catch (error) {
-      return console.log(error);
-    }
-  }
-  async fetchTrending() {
-    const url = `${BASE_URL}${URL_TRENDING}?${searchParams}`;
+    const url = `${BASE_URL}${URL_TRENDING}?${searchParams}`;
+    console.log(url);
+    try {
+      const response = await axios.get(url);
+      this.setQuery('');
+      this.resetPage();
+      this.setLoadedHits(response.data.total_pages);
+      hiddenSpinner(true);
+      return response.data;
+    } catch (error) {
+      hiddenSpinner(true);
+      return console.log(error);
+    }
+  }
+  async fetchTrending() {
+    hiddenSpinner(false);
+    const url = `${BASE_URL}${URL_TRENDING}?${searchParams}`;
 
-    try {
-      const response = await axios.get(url);
+    try {
+      const response = await axios.get(url);
 
-      this.setQuery('');
-      this.resetPage();
-      this.setLoadedHits(response.total_pages);
-      // console.log(response.data);
-      return response.data;
-    } catch (error) {
-      return console.log(error);
-    }
-  }
+      this.setQuery('');
+      this.resetPage();
+      this.setLoadedHits(response.total_pages); // console.log(response.data);
+      hiddenSpinner(true);
+      return response.data;
+    } catch (error) {
+      hiddenSpinner(true);
+      return console.log(error);
+    }
+  }
 
-  async fetchKey(query) {
-    const url = `${BASE_URL}${URL_KEY}?${searchParams}&query=${query}`;
+  async fetchKey(query) {
+    hiddenSpinner(false);
+    const url = `${BASE_URL}${URL_KEY}?${searchParams}&query=${query}`;
 
-    try {
-      const response = await axios.get(url);
+    try {
+      const response = await axios.get(url);
 
-      this.setQuery(query);
-      this.resetPage();
-      this.setLoadedHits(response.total_pages);
+      this.setQuery(query);
+      this.resetPage();
+      this.setLoadedHits(response.total_pages);
+      hiddenSpinner(true);
+      return response.data;
+    } catch (error) {
+      hiddenSpinner(true);
+      return console.log(error);
+    }
+  }
 
-      return response.data;
-    } catch (error) {
-      return console.log(error);
-    }
-  }
+  async fullFetch(id) {
+    const url = `${BASE_URL}${URL_ID}/${id}?${searchParams}`;
 
-  async fullFetch(id) {
-    const url = `${BASE_URL}${URL_ID}/${id}?${searchParams}`;
+    try {
+      const response = await axios.get(url);
 
-    try {
-      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      return console.log(error);
+    }
+  }
 
-      return response.data;
-    } catch (error) {
-      return console.log(error);
-    }
-  }
+  incrementPage(nextPage) {
+    this.page = nextPage;
+  }
 
-  incrementPage(nextPage) {
-    this.page = nextPage;
-  }
+  resetPage() {
+    this.page = 1;
+  }
 
-  resetPage() {
-    this.page = 1;
-  }
+  getQuery() {
+    return this.searchQuery;
+  }
 
-  getQuery() {
-    return this.searchQuery;
-  }
+  setQuery(newQuery) {
+    this.searchQuery = newQuery;
+  }
 
-  setQuery(newQuery) {
-    this.searchQuery = newQuery;
-  }
+  getLoadedHits() {
+    return this.loadedHits;
+  }
 
-  getLoadedHits() {
-    return this.loadedHits;
-  }
-
-  setLoadedHits(hits) {
-    this.loadedHits = hits;
-  }
+  setLoadedHits(hits) {
+    this.loadedHits = hits;
+  }
 }
