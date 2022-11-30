cdcbc7a756d80ac5a80d8c39c9d156225566c4fd
Julia Rudenko <yrudenko@i.ua>
2022-11-30 11:17:02 +0200

    Правка фона main

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -4,7 +4,8 @@ export const refs = {
   formInput: document.querySelector('.header-searchform__input'),
   formButton: document.querySelector('.header-searchform__button'),
   spinner: document.querySelector('.spinner__container'),
-  divBackdrop: document.querySelector('.backdrop'),
+  // divBackdrop: document.querySelector('.backdrop'),
+  divBackdrop: document.querySelector('#modalSingleMovie'),
   buttonlose: document.querySelector('.singleMovieModal__button-close'),
   alertMessage: document.querySelector('.header__warning-text'),
 };
