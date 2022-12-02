d0dc3de8f2840c360cb8d38bb8fe5f15887529f4
Andrii Sereda <a.a.sereda@ukr.net>
2022-12-02 20:00:26 +0200

    ready treiler container

--- src/js/components/modalWindow/trailer.js
+++ src/js/components/modalWindow/trailer.js
@@ -1,8 +1,35 @@
 import { refs } from '../../refs/refs';
 
+function onCloseVideo() {
+  refs.treilerContainer.innerHTML = '';
+
+  window.removeEventListener('keydown', onEscKeyPress);
+
+  refs.treilerButtonClose.removeEventListener('click', onCloseButtonPress);
+  refs.treilerButtonClose.classList.add('is-hidden');
+}
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    console.log('Stoped video');
+    onCloseVideo();
+  }
+}
+
+function onCloseButtonPress() {
+  onCloseVideo();
+}
+
 export function appendMarkupTrailer(key) {
-  console.log(key);
+  window.addEventListener('keydown', onEscKeyPress);
+
+  refs.treilerButtonClose.classList.remove('is-hidden');
+  refs.treilerButtonClose.addEventListener('click', onCloseButtonPress);
+
   refs.treilerContainer.innerHTML = `
-  <iframe class="trailer__iframe" width="700" height="500" src="https://www.youtube.com/embed/${key}" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay="1"; controls="1"; clipboard-write; encrypted-media; gyroscope="1"; picture-in-picture"; allowfullscreen="1"; enablejsapi="1"></iframe>
+  <iframe class="trailer__iframe" width="700" height="500" src="https://www.youtube.com/embed/${key}" title="YouTube video player" frameborder="1" allow="accelerometer;  controls="1"; clipboard-write; encrypted-media; gyroscope="1"; picture-in-picture"; allowfullscreen="1"; enablejsapi="1"></iframe>
   `;
 }
