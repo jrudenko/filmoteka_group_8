6372bb88ceef13ec397a37297f690ecd1ccab368
Andrii Sereda <a.a.sereda@ukr.net>
2022-12-04 16:15:39 +0200

    fixed more css bugs

--- src/js/templates/movieCard.js
+++ src/js/templates/movieCard.js
@@ -38,7 +38,7 @@ function movieCardTemplate({
   return `
     <div class="singleMovieModal__container">
     <button class="singleMovieModal__button-close" type="button" id="button-close">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg"
+      <svg  class="singleMovieModal__button-close-img" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg"
         viewBox="0 0 16 16">
         <path
           d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
@@ -70,7 +70,10 @@ function movieCardTemplate({
             <p class="singleMovieModal__description-span">${genreses}</p>
           </li>
         </ul>
-        <p class="singleMovieModal__about">
+          <svg class="singleMovieModal__about-more-button" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
+          </svg>
+        <p class="singleMovieModal__about singleMovieModal__about-more-button-active">
           ${overview}
         </p>
         <ul class="singleMovieModal-add__list">
